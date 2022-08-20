fbb97636bc0199a766b3165ef9a8e7aed48d474c
tp <61617660+ittp@users.noreply.github.com>
2022-08-20 19:31:33 +0300

    Update hello.ts

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,6 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+
+type Mount = {
+
+  dev: "/dev",
+  proc: "/proc",
+  sys: "/sys"
+}
+
 type Data = {
   name: string
 }
